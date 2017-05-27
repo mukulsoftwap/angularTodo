6b311b1d2f6e77d36201c8d832d1e2182922e99c
Mukul Sharma <mukulsoftwap@gmail.com>
2017-05-27 16:51:26 +0530

    All Client Code
    Simple Client Code

---
+++ angularTodo/client/app/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('demo',[]);
+
+app.controller('demoCtrl', demoCtrl);
+
+function demoCtrl($scope){
+    $scope.todos = [];
+    $scope.index = -1;
+
+    $scope.addTodo = function(){
+        if($scope.todo!="" && $scope.todo!=null && $scope.todo!=undefined && $scope.index==-1){
+            $scope.todos.push($scope.todo);
+        }
+        else if($scope.index!=-1 && $scope.index!=undefined){
+            $scope.todos[$scope.index] = $scope.todo;
+        }
+        $scope.todo = "";
+    }
+
+    $scope.deleteTodo = function(index){
+        $scope.todos.splice(index,1);
+    }
+
+    $scope.editTodo = function(val, indx){
+        $scope.todo = val;
+        $scope.index = indx;
+    }
+
+}
